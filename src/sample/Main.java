7c0c1eb2b879ca090d20847318d0ab1712c3c6b6
Erol Yapıcı <eyapici@ebay.com>
2015-05-21 11:39:22 +0300

    Canvas Example

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,20 +1,48 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
-import java.security.acl.Group;
+import javafx.scene.canvas.Canvas;
+
+import java.net.URL;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Canvas Example");
+
+        Group root = new Group();
+        Scene theScene = new Scene(root);
+        primaryStage.setScene(theScene);
+
+        Canvas canvas = new Canvas(400, 200);
+        root.getChildren().add(canvas);
+
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+
+        gc.setFill(Color.RED);
+        gc.setStroke(Color.BLACK);
+        gc.setLineWidth(2);
+        Font theFont = Font.font("Times New Roman", FontWeight.BOLD, 48);
+        gc.setFont(theFont);
+        gc.fillText("Hello Word!", 60, 50);
+        gc.strokeText("Hello Word!", 60,50);
+
+        URL path = getClass().getClassLoader().getResource("resources/earth.jpeg");
+        if(path != null) {
+            Image earth = new Image(path.toExternalForm());
+            gc.drawImage(earth, 180, 100);
+        }
+
         primaryStage.show();
     }
 
